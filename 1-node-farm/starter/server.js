b66cf94d2377208d9950b890b4ce5d459ca4a9c4
vagman <bmanousakis@gmail.com>
2025-07-29 01:20:36 +0300

    http responses

--- 1-node-farm/starter/server.js
+++ 1-node-farm/starter/server.js
@@ -1,14 +1,22 @@
 import http from 'node:http';
-import url from 'node:url';
+import { readFileSync } from 'node:fs';
+import { fileURLToPath } from 'node:url';
 
 // Server Routing
+const dataPath = fileURLToPath(import.meta.resolve('./dev-data/data.json'));
+const data = readFileSync(dataPath, 'utf-8');
+const dataObject = JSON.parse(data);
+
 const server = http.createServer((request, response) => {
-  const pathName = request.url;
+    const pathName = request.url;
     if (pathName === '/' || pathName === '/overview') {
         response.end('This is the OVERVIEW');
     } else if (pathName === '/product') {
         response.end('This is the PRODUCT');
-    } else {
+    } else if (pathName ==='/api') {
+        response.writeHead(200, {'Content-type': 'application/json'});
+        response.end(data);
+    }else {
         response.writeHead(404, {
             'Content-type': 'text/html',
             'my-own-header': 'hello-world'
