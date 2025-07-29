d25b770a441ba6e2a40ad3aaea0c3d598236ca6e
vagman <bmanousakis@gmail.com>
2025-07-29 22:33:27 +0300

    Export function to ES6 Modoule

---
+++ 1-node-farm/starter/modules/replaceTemplate.js
@@ -0,0 +1,14 @@
+const replaceTemplate = (template, product) => {
+    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTSNAME%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%ID%}/g, product.id);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+    return output;
+};
+
+export default replaceTemplate;
--- 1-node-farm/starter/server.js
+++ 1-node-farm/starter/server.js
@@ -1,21 +1,9 @@
 import http from 'node:http';
 import { readFileSync } from 'node:fs';
 import { fileURLToPath, URL } from 'node:url';
+import replaceTemplate from './modules/replaceTemplate.js';
 
 // Server Routing
-const replaceTemplate = (template, product) => {
-    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTSNAME%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%ID%}/g, product.id);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
-    return output;
-};
-
 const templateOverview = readFileSync(fileURLToPath(import.meta.resolve('./templates/template-overview.html')), 'utf-8');
 const templateProduct = readFileSync(fileURLToPath(import.meta.resolve('./templates/template-product.html')), 'utf-8');
 const templateCard = readFileSync(fileURLToPath(import.meta.resolve('./templates/template-card.html')), 'utf-8');
