857a05b863c83bcb00feefc2266fff083486f58d
Vaggelis Manousakis <bmanousakis@gmail.com>
2025-08-15 18:24:31 +0300

    update and delete routes implemented

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -67,6 +67,40 @@ app.post('/api/v1/tours', (request, response) => {
   );
 });
 
+// Update route
+app.patch('/api/v1/tours/:id', (request, response) => {
+  // Converting the id from string to number and checking if the specific tour exists
+  if (request.params.id * 1 > tours.length) {
+    return response.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  response.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+});
+
+// Delete route
+app.delete('/api/v1/tours/:id', (request, response) => {
+  // Converting the id from string to number and checking if the specific tour exists
+  if (request.params.id * 1 > tours.length) {
+    return response.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  response.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
