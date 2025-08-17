1472e2fbaf46623034e07e7c445cfbe36f64c903
Vaggelis Manousakis <bmanousakis@gmail.com>
2025-08-18 02:06:48 +0300

    environment variables

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -9,7 +9,8 @@ const app = express();
 
 // ------------ 1) Middleware Functions -------------
 // Middleware: function that modifies the incoming request data
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development')
+  app.use(morgan('dev'));
 app.use(express.json());
 
 const __dirname = dirname(
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,7 +1,9 @@
 import app from './app.js';
+import dotenv from 'dotenv';
+dotenv.config({ path: './config.env' });
 
 // ------------- 4) Start Server -------------
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
