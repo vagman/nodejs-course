833e3ee236f49f56f65cb343994aa8844c6a26b8
Vaggelis Manousakis <bmanousakis@gmail.com>
2025-08-20 16:15:44 +0300

    creating documents with mongoose and testing a db table model

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -9,10 +9,40 @@ const dbConnectionString =
     process.env.DATABASE_PASSWORD,
   );
 
-mongoose
-  .connect(dbConnectionString)
-  .then((connection) => {
-    console.log('DB connection successful!');
+mongoose.connect(dbConnectionString).then(() => {
+  console.log('DB connection successful!');
+});
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+const testTour = new Tour({
+  name: 'The Park Camper',
+  rating: 4.7,
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((document) => {
+    console.log(document);
+  })
+  .catch((error) => {
+    console.error(`ERROR: ${error} 💣`);
   });
 
 // ------------- 4) Start Server -------------
