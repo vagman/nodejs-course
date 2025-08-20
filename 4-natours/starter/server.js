440b82688b9a11819cd739f853aa0b3eb7b2fe24
Vaggelis Manousakis <bmanousakis@gmail.com>
2025-08-20 14:34:33 +0300

    connection with MongoDB cloud

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,19 @@
 import app from './app.js';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
 dotenv.config({ path: './.env' });
+const dbConnectionString =
+  process.env.DATABASE.replace(
+    'PASSWORD',
+    process.env.DATABASE_PASSWORD,
+  );
+
+mongoose
+  .connect(dbConnectionString)
+  .then((connection) => {
+    console.log('DB connection successful!');
+  });
 
 // ------------- 4) Start Server -------------
 const port = process.env.PORT || 3000;
