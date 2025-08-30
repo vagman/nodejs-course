129f2ef7b63cb459da99bf6136ce0eab688e6723
Vaggelis Manousakis <bmanousakis@gmail.com>
2025-08-30 16:16:47 +0300

    mongoose virtual properties

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -169,15 +169,45 @@ const getTourStats = async (request, response) => {
 
 const getMonthlyPlan = async (request, response) => {
   try {
-    const year = request.params.year * 1;
+    const year = request.params.year * 1; // 2021
     const plan = await Tour.aggregate([
       {
         $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: { month: '$_id' },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: { numTourStarts: -1 },
+      },
+      {
+        $limit: 12,
       },
     ]);
 
     response.status(200).json({
       status: 'success',
+      result: plan.length,
       data: {
         plan,
       },
