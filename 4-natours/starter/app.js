038ccc4cb57d227dcad3e72f5cea4176ca41befe
Vaggelis Manousakis <bmanousakis@gmail.com>
2025-09-02 00:55:51 +0300

    implementing global error handling middleware

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -30,9 +30,29 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('/{*any}', (request, response, next) => {
-  response.status(404).json({
-    status: 'Fail',
-    message: `Can't find ${request.originalUrl} on this server!`,
+  // response.status(404).json({
+  //   status: 'Fail',
+  //   message: `Can't find ${request.originalUrl} on this server!`,
+  // });
+
+  const error = new Error(
+    `Can't find ${request.originalUrl} on this server!`,
+  );
+  error.status = 'fail';
+  error.statusCode = 404;
+
+  next(error);
+});
+
+// ERROR-HANDLING MIDDLEWARE
+app.use((error, request, response, next) => {
+  console.log(error.statusCode);
+  response.status(error.statusCode || 500);
+  error.status = error.status || 'error';
+
+  response.status(error.statusCode).json({
+    status: error.status,
+    message: error.message,
   });
 });
 
