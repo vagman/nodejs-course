0403c9a3801607675cdba2c8c0a905fd692036ef
Vaggelis Manousakis <bmanousakis@gmail.com>
2025-09-08 15:39:14 +0300

    signing users with jsonwebtoken

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -1,17 +1,26 @@
+import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js';
 import catchAsync from '../utils/catchAsync.js';
 
-const signup = catchAsync(
-  async (request, response, next) => {
-    const newUser = await User.create(request.body);
+const signup = catchAsync(async (request, response, next) => {
+  const newUser = await User.create({
+    name: request.body.name,
+    email: request.body.email,
+    password: request.body.password,
+    passwordConfirm: request.body.passwordConfirm,
+  });
 
-    response.status(201).json({
-      status: 'success',
-      data: {
-        user: newUser,
-      },
-    });
-  },
-);
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
+  response.status(201).json({
+    status: 'success',
+    token,
+    data: {
+      user: newUser,
+    },
+  });
+});
 
 export { signup };
