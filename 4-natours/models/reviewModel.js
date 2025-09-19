941238536493aabbe92d8cfeafc6f14707c0471b
Vaggelis Manousakis <bmanousakis@gmail.com>
2025-09-19 13:07:58 +0300

    modeling reviews with parent referencing

---
+++ 4-natours/models/reviewModel.js
@@ -0,0 +1,43 @@
+// Challenge: create the Review Model: review / rating / createdAt / ref to tour / ref to user
+import mongoose from 'mongoose';
+import { Schema } from 'mongoose';
+
+const reviewSchema = new Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review can not be empty!'],
+      trim: true,
+      maxlength: [500, 'A review must have less or equal than 500 characters'],
+      minlength: [10, 'A review must have more or equal than 10 characters'],
+    },
+    rating: {
+      type: Number,
+      required: [true, 'Rating can not be empty!'],
+      min: [1, 'Rating must be above 1'],
+      max: [5, 'Rating must be below 5'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour.'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user.'],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+export default Review;
