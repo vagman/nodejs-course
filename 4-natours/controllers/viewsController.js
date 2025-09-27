48982ec75f9384dc4a40bba44abc95511c6b2f78
Vaggelis Manousakis <bmanousakis@gmail.com>
2025-09-27 09:09:01 +0300

    building the tour overview

--- 4-natours/controllers/viewsController.js
+++ 4-natours/controllers/viewsController.js
@@ -1,8 +1,19 @@
-const getOverview = (request, response) => {
+import Tour from '../models/tourModel.js';
+import catchAsync from '../utils/catchAsync.js';
+
+const getOverview = catchAsync(async (request, response, next) => {
+  // 1. Get tour data from collection
+  const tours = await Tour.find();
+
+  // 2. Build template
+
+  // 3. Render that template using tour data from Step 1.
+
   response.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
 const getTour = (request, response) => {
   response.status(200).render('tour', {
