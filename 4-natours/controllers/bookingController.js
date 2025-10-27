6e77438f7ca5ee5892885d046c362fb79ea5ce4d
vagman <bmanousakis@gmail.com>
2025-10-27 20:18:35 +0200

    intergrating Stripe into the back-end

--- 4-natours/controllers/bookingController.js
+++ 4-natours/controllers/bookingController.js
@@ -1,19 +1,46 @@
-import stripe from 'stripe';
+import Stripe from 'stripe';
 
 import Tour from '../models/tourModel.js';
 import catchAsync from '../utils/catchAsync.js';
 import * as factory from './handlerFactory.js';
 import AppError from '../utils/appError.js';
 
-const getCheckoutSession = catchAsync( async (request, response, next) => {
-  // 1.Get thecurrently booked tour
-    const tour = await Tour.findById(request.params.tourId);
+const getCheckoutSession = catchAsync(async (request, response, next) => {
+  // 0. Get thecurrently booked tour
+  const tour = await Tour.findById(request.params.tourId);
+
+  // 1. Create stripe checkout session
+  // Adding this line in top-level code was producing error: "Neither apiKey nor config.authenticator provided" read more:https://stackoverflow.com/questions/79086035/firebase-deploy-error-neither-apikey-nor-config-authenticator-provided-using-s/
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
   // 2. Create checkout session
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    customer_email: request.user.email,
+    client_reference_id: request.params.tourId, // to identify the tour
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          unit_amount: tour.price * 100,
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+          },
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${request.protocol}://${request.get('host')}/`,
+    cancel_url: `${request.protocol}://${request.get('host')}/tour/${tour.slug}`,
+  });
 
   // 3. Create session as response (send the session back to the client)
   response.status(200).json({
     status: 'success',
-    message: 'This is a placeholder for the checkout session.',
+    session,
   });
 });
 
--- 4-natours/routes/bookingRoutes.js
+++ 4-natours/routes/bookingRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import { getCheckoutSession } from '../controllers/bookingController.js';
-import { protect, restrictTo } from '../controllers/authController.js';
+import { protect } from '../controllers/authController.js';
 
 const router = express.Router({});
 
-router.get('/checkout-session/:tourID', protect, getCheckoutSession);
+router.get('/checkout-session/:tourId', protect, getCheckoutSession);
 
 export default router;
