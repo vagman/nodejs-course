bbdae8fa7f5899d04683e92955aefa5def41222f
vagman <bmanousakis@gmail.com>
2025-11-02 23:37:17 +0200

    Modelling the Tour Bookings

---
+++ 4-natours/models/bookingModel.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const bookingSchema = new mongoose.Schema({
+  tour: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Tour',
+    required: [true, 'A booking must belong to a tour!'],
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'A booking must belong to a user!'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  paid: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+bookingSchema.pre(/^find/, function (next) {
+  this.populate('user').populate({ path: 'tour', select: 'name' });
+  next();
+});
+
+const Booking = mongoose.model('Booking', bookingSchema);
+
+export default Booking;
